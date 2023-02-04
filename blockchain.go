ec277f62d83f556afb954841d32f0f835f562267
snowwhitedev <snowwhite91924@gmail.com>
2023-02-04 21:14:07 +0700

    feat: setup

---
+++ blockchain.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type Block struct {
+	nonce        int
+	previousHash string
+	timestamp    int64
+	transactions []string
+}
+
+func NewBlock(nonce int, previousHash string) *Block {
+	b := new(Block)
+	b.timestamp = time.Now().UnixNano()
+	b.nonce = nonce
+	b.previousHash = previousHash
+	return b
+}
+
+func main() {
+	b := NewBlock(0, "init hash")
+	fmt.Println(b)
+}
